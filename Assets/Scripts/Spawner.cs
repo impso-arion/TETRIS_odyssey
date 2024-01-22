1615c383a9ba6fc082c5a8d2c428504e1b3ea22f
Saiga tatarica <85770503+impso-arion@users.noreply.github.com>
2024-01-22 13:30:53 +0900

    WebGL公開

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using Unity.VisualScripting.Antlr3.Runtime.Tree;
-using UnityEditor.Tilemaps;
+//using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
